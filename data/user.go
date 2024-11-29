9864d786d6aef18265e17e5b7e517f8636854388
Sophia <65602967+Sophed@users.noreply.github.com>
2024-11-29 15:53:24 +0000

    fix broken json tags

--- data/user.go
+++ data/user.go
@@ -6,7 +6,7 @@ type User struct {
 	DisplayName    string `json:"display_name" bson:"display_name"`
 	Email          string `json:"email" bson:"email"`
 	ProfilePicture string `json:"pfp" bson:"pfp"`
-	PasswordHash   string `json:"pfp" bson:"password_hash"`
-	PasswordSalt   string `json:"pfp" bson:"password_salt"`
+	PasswordHash   string `json:"password_hash" bson:"password_hash"`
+	PasswordSalt   string `json:"password_salt" bson:"password_salt"`
 	Timestamp      int64  `json:"timestamp" bson:"timestamp"`
 }

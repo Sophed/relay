0768e3173c7d0b9bbad991c925e08c372a8e8fa9
Sophia <65602967+Sophed@users.noreply.github.com>
2025-03-24 15:09:33 +0000

    log logouts

--- cmd/internal/api/logout.go
+++ cmd/internal/api/logout.go
@@ -1,14 +1,21 @@
 package api
 
 import (
+	"messaging/auth"
 	"messaging/data/sessions"
 	"time"
 
+	"github.com/sophed/lg"
 	"github.com/gofiber/fiber/v2"
 )
 
 // handles web requests to /api/logout
 func Logout(c *fiber.Ctx) error {
+	// check auth
+	authed, user := auth.Validate(c)
+	if authed {
+		lg.Info("[logout] " + user.Username + " - " + user.ID) // log user login
+	}
 	c.Set("HX-Redirect", "/") // redirect to /
 	c.Cookie(&fiber.Cookie{   // send an empty cookie to override existing sessions
 		Name:     sessions.SESSION_COOKIE_KEY,
